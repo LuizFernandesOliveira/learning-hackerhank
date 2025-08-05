4f734c46734d90019cd35ad222e90840e0f20e48
Luiz Fernandes de Oliveira <36059306+LuizFernandesOliveira@users.noreply.github.com>
2025-08-05 01:59:53 +0000

    java string regex

---
+++ java/strings/JavaRegex.java
@@ -0,0 +1,8 @@
+
+
+//Write your code here
+class JavaRegex {
+    // IP address is a string in the form "A.B.C.D", where the value of A, B, C, and D may range from 0 to 255. Leading zeros are allowed. 
+    // The length of A, B, C, or D can't be greater than 3.
+    public String pattern = "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
+}
