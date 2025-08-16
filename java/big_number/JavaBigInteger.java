355f2a245382c4ccf394be8e4e9e55b5c0521c01
Luiz Fernandes de Oliveira <36059306+LuizFernandesOliveira@users.noreply.github.com>
2025-08-16 02:20:37 +0000

    java big numbers big intenger

---
+++ java/big_number/JavaBigInteger.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class JavaBigInteger {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        BigInteger n1 = new BigInteger(sc.next());
+        BigInteger n2 = new BigInteger(sc.next());
+        
+        System.out.println(n1.add(n2));
+        System.out.println(n1.multiply(n2));
+    }
+}
