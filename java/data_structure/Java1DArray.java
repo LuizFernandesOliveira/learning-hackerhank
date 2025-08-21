934873d7f284a558b97177150440d3f351954344
Luiz Fernandes de Oliveira <36059306+LuizFernandesOliveira@users.noreply.github.com>
2025-08-21 00:21:03 +0000

    java data structure 1d array

---
+++ java/data_structure/Java1DArray.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Java1DArray {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+            
+        for (int i = 0; i < n; i++) {
+            List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+            
+            System.out.println(a.get(0));
+        }
+        
+        bufferedReader.close();
+    }
+}
